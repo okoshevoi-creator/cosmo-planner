42e01b77b01d32eef46e8869d5eb0a2f07a8bf3d
okoshevoi-creator <okoshevoi@gmail.com>
2026-01-03 15:11:08 +0200

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.lovable.604a7c2b0f5948ad8222bd3a5d0769fc',
+  appName: 'Beauty Cabinet',
+  webDir: 'dist',
+  server: {
+    url: 'https://604a7c2b-0f59-48ad-8222-bd3a5d0769fc.lovableproject.com?forceHideBadge=true',
+    cleartext: true
+  }
+};
+
+export default config;
